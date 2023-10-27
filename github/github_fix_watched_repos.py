f7cb66a649be91d94246f9304fcb13813ccd426b
Matt Wyen <matt@mattwyen.me>
2023-10-27 11:13:52 -0400

    update

--- github/github_fix_watched_repos.py
+++ github/github_fix_watched_repos.py
@@ -14,6 +14,19 @@ import requests
 # 10. After generating the token, make sure to copy it. You won't be able to see it again!
 # Remember to keep your tokens secret; treat them just like passwords. If a token is ever compromised, you can go back to the token settings and revoke it.
 
+print("To generate a personal access token in GitHub:\n\
+1. Sign in to your GitHub account.\n\
+2. Click on your profile photo in the upper-right corner of any page.\n\
+3. Click on `Settings` in the drop-down menu.\n\
+4. In the left sidebar, click on `Developer settings`.\n\
+5. Click on `Personal access tokens`.\n\
+6. Click on `Generate new token`.\n\
+7. Give your token a descriptive name in the `Note` field.\n\
+8. Select the scopes, or permissions, you'd like to grant this token. For this script, you'll need at least the `repo` scope.\n\
+9. Click `Generate token`.\n\
+10. After generating the token, make sure to copy it. You won't be able to see it again!\n\
+Remember to keep your tokens secret; treat them just like passwords. If a token is ever compromised, you can go back to the token settings and revoke it.")
+
 # Prompt the user for their GitHub username and personal access token
 username = input('Enter your GitHub username: ')
 token = getpass.getpass('Enter your GitHub token: ')
