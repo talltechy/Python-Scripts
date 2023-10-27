eeeb555a042b61106d81a6c42bf773dc361a34d7
Matt Wyen <matt@mattwyen.me>
2023-10-27 13:11:41 -0400

    update

--- github/github_fix_watched_repos.py
+++ github/github_fix_watched_repos.py
@@ -14,22 +14,24 @@ log_file = os.path.join(os.path.dirname(__file__), 'log.txt')
 logging.basicConfig(filename=log_file, level=logging.INFO)
 
 print(Fore.GREEN + "To generate a personal access token in GitHub:\n"
-      "1. Sign in to your GitHub account.\n"
-      "2. Click on your profile photo in the upper-right corner of any page.\n"
-      "3. Click on `Settings` in the drop-down menu.\n"
-      "4. In the left sidebar, click on `Developer settings`.\n"
-      "5. Click on `Personal access tokens`.\n"
-      "6. Click on `Generate new token`.\n"
-      "7. Give your token a descriptive name in the `Note` field.\n"
-      "8. Select the scopes, or permissions, you'd like to grant this token.\n"
-      "   For this script, you'll need at least the `repo` scope.\n"
-      "9. Click `Generate token`.\n"
-      "10. After generating the token, make sure to copy it. You won't be able to see it again!\n"
-      "Remember to keep your tokens secret; treat them just like passwords.\n"
-      "If a token is ever compromised, you can go back to the token settings and revoke it." + Style.RESET_ALL)
+    "1. Sign in to your GitHub account.\n"
+    f"2. Click on your profile photo in the upper-right corner of any page.\n"
+    f"3. Click on {Fore.YELLOW}'Settings'{Style.RESET_ALL} in the drop-down menu.\n"
+    f"4. In the left sidebar, click on {Fore.YELLOW}'Developer settings'{Style.RESET_ALL}.\n"
+    f"5. Click on {Fore.YELLOW}'Personal access tokens'{Style.RESET_ALL}.\n"
+    f"6. Click on {Fore.YELLOW}'Generate new token'{Style.RESET_ALL}.\n"
+    f"7. Give your token a descriptive name in the {Fore.YELLOW}'Note'{Style.RESET_ALL} field.\n"
+    f"8. Select the scopes, or permissions, you'd like to grant this token.\n"
+    f"   For this script, you'll need at least the {Fore.YELLOW}'repo'{Style.RESET_ALL} scope.\n"
+    f"9. Click {Fore.YELLOW}'Generate token'{Style.RESET_ALL}.\n"
+    "10. After generating the token, make sure to copy it. You won't be able to see it again!\n"
+    "Remember to keep your tokens secret; treat them just like passwords.\n"
+    "If a token is ever compromised, you can go back to the token settings and revoke it." + 
+    Style.RESET_ALL)
 
 # Ask the user if they want to open the URL to create a personal access token in a browser
-open_url = input(Fore.YELLOW + 'Do you want to open the URL to create a personal access token in a browser? (y/n): ' + Style.RESET_ALL)
+open_url = input(Fore.YELLOW + 'Do you want to open the URL to create a personal access token in a browser? (y/n): ' + 
+                 Style.RESET_ALL)
 if open_url.lower() == 'y':
     webbrowser.open('https://github.com/settings/tokens/new')
 
@@ -50,7 +52,7 @@ headers = {
 # Get the list of repositories you're watching
 response = requests.get(f'https://api.github.com/users/{username}/subscriptions',
                         headers=headers, timeout=10)
-repos = response.json()['items']
+repos = response.json()
 
 # Loop over the repositories
 for repo in repos:
@@ -64,9 +66,12 @@ for repo in repos:
 
     # Prompt the user for subscription settings
     print(Fore.CYAN + f'Subscription settings for {repo_name}:' + Style.RESET_ALL)
-    subscription_settings['subscribed'] = input(Fore.YELLOW + 'Subscribe to notifications? (y/n): ' + Style.RESET_ALL).lower() == 'y'
-    subscription_settings['ignored'] = input(Fore.YELLOW + 'Ignore notifications? (y/n): ' + Style.RESET_ALL).lower() == 'y'
-    subscription_settings['reason'] = input(Fore.YELLOW + 'Reason for notifications (e.g. releases, all, etc.): ' + Style.RESET_ALL)
+    subscription_settings['subscribed'] = input(Fore.YELLOW + 'Subscribe to notifications? (y/n): ' + 
+                                                 Style.RESET_ALL).lower() == 'y'
+    subscription_settings['ignored'] = input(Fore.YELLOW + 'Ignore notifications? (y/n): ' + 
+                                              Style.RESET_ALL).lower() == 'y'
+    subscription_settings['reason'] = input(Fore.YELLOW + 'Reason for notifications (e.g. releases, all, etc.): ' + 
+                                             Style.RESET_ALL)
 
     response = requests.put(subscription_url, headers=headers, json=subscription_settings, timeout=10)
 
